d2869a771338be14197f7103a52d1ab04313b53c
Maxim Odorodko <modorodko@it-one.ru>
2021-12-25 10:49:20 +0300

    init commit

---
+++ src/index.test.js
@@ -0,0 +1,29 @@
+const sum = require('./index');
+
+describe('функция суммы', () => {
+    test('работает как задумано', () => {
+        expect(sum(1, 2)).toBe(3);
+    });
+
+    test('работает как задумано 2', () => {
+        expect(sum(1, 2)).not.toBe(4);
+    });
+
+    test('матчинг объектов', () => {
+        expect({
+            test: 1,
+            testTest: 2,
+            monkey: 'jump',
+            foo: 'bar',
+        }).toMatchObject({test: 1});
+    });
+
+    test('матчинг объектов наоборот', () => {
+        expect({test: 1}).not.toMatchObject({
+            test: 1,
+            testTest: 2,
+            monkey: 'jump',
+            foo: 'bar',
+        });
+    })
+})
