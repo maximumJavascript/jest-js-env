d58b8b5a0a411aa8e761fa94327cf3a9d69078de
Katana <katanaosu@vk.com>
2022-01-17 17:31:15 +0300

    Resolved issues about function of prototype

--- src/taskClassesSerialize/serializeClassInstanceData.js
+++ src/taskClassesSerialize/serializeClassInstanceData.js
@@ -2,77 +2,28 @@
  * @param instance class instance to serialize
  * @returns plain object
  */
-import { ForestBeast, HairedForestBeast, HairedForestBeastWithFamily } from "./codebase";
 
-const constructForestBeastInstance = (child, parent) => {
-	Object.defineProperties(child, {
-		countOfLimbs: {
-			value: parent.countOfLimbs,
-			enumerable: true,
-		},
-		name: {
-			value: parent.name,
-			enumerable: true,
-		},
-		geneticData: {
-			value: null,
-			enumerable: true,
-		},
-		id: {
-			value: parent.id,
-		}
-	});
-}
+import {ForestBeast, HairedForestBeast, HairedForestBeastWithFamily} from "./codebase";
 
-const constructHairedForestBeastInstance = (child, parent) => {
-	Object.defineProperties(child, {
-		countOfLimbs: {
-			value: parent.countOfLimbs,
-			enumerable: true,
-		},
-		name: {
-			value: parent.name,
-			enumerable: true,
-		},
-		id: {
-			value: parent.id,
-		},
-		hasHair: {
-			value: parent.hasHair,
-			enumerable: true,
-		},
-		isSocial: {
-			value: parent.isSocial,
-			enumerable: true,
-		}
-	});
+const constructAnyInstance = (descs) => {
+    const obj = Object.create(Object.prototype);
+
+    for(let [key, value] of Object.entries(descs)){
+        // if(value.get){
+        //     obj[key] = value.get();
+        // }
+        //TODO: разобраться, какие свойства нужны, а также, что у них общего
+        console.log(key, value);
+    }
+    return obj;
 }
 
 const serializeClassInstanceData = (instance) => {
-
-	if (instance.constructor === ForestBeast) {
-		const obj = Object.create(Object.prototype);
-		constructForestBeastInstance(obj, instance);
-		return obj;
-	}
-
-	if (instance.constructor === HairedForestBeast) {
-		const obj = Object.create(Object.prototype);
-		constructHairedForestBeastInstance(obj, instance);
-		return obj;
-	}
-
-	if (instance.constructor === HairedForestBeastWithFamily) {
-		const obj = Object.create(Object.prototype);
-		const family = instance.family;
-		obj.family = [];
-		for (let i = 0; i < family.length; i++) {
-			const member = Object.create(Object.prototype);
-			constructHairedForestBeastInstance(member, family[i]);
-			obj.family.push(member);
-		}
-		return obj;
-	}
+    // console.log(instance.name)
+    // console.log(Object.getPrototypeOf(instance));
+    const descriptors = Object.getOwnPropertyDescriptors(instance.constructor.prototype);
+    // console.log(Object.getOwnPropertyNames(instance.constructor.prototype));
+    console.log(constructAnyInstance(descriptors));
 }
 
 export default serializeClassInstanceData;
