4344bfb5c9086e0cca8a458cf1572260eef20e8c
Katana <katanaosu@vk.com>
2022-01-18 18:29:29 +0300

    Resolve second test but have issues with first one; TODO: to refactor repeating code

--- src/taskClassesSerialize/serializeClassInstanceData.js
+++ src/taskClassesSerialize/serializeClassInstanceData.js
@@ -3,26 +3,35 @@
  * @returns plain object
  */
 
-const constructAnyInstance = (descs, parent) => {
+const serializeClassInstanceData = (instance) => {
+
+    const instanceConstructor = instance.constructor;
+    const descriptors = Object.getOwnPropertyDescriptors(instanceConstructor.prototype);
+    const constructorProto = Object.getPrototypeOf(instanceConstructor);
+    const descriptors2 = Object.getOwnPropertyDescriptors(constructorProto.prototype)
+
     const obj = Object.create(Object.prototype);
 
-    for (let [key, value] of Object.entries(descs)) {
-
-        if (value.get && value.get.call(parent) !== undefined) {
+    for (const [key, value] of Object.entries(descriptors)) {
+        if (value.get && value.get.call(instance) !== undefined) {
             Object.defineProperty(obj, key, {
-                value: value.get.call(parent),
+                value: value.get.call(instance),
                 enumerable: true,
+                configurable: true,
             });
         }
     }
+    if (instance instanceof constructorProto) {
+        for (const [key, value] of Object.entries(descriptors2)) {
+            if (value.get && (value.get.call(instance) !== undefined) && (!obj[key])) {
+                Object.defineProperty(obj, key, {
+                    value: value.get.call(instance),
+                    enumerable: true,
+                });
+            }
+        }
+    }
     return obj;
-}
-
-const serializeClassInstanceData = (instance) => {
-
-    const descriptors = Object.getOwnPropertyDescriptors(instance.constructor.prototype);
-    // console.log(Object.getOwnPropertyNames(instance.constructor.prototype));
-    return constructAnyInstance(descriptors, instance);
 }
 
 export default serializeClassInstanceData;
